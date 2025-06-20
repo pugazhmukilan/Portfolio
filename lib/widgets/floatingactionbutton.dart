b668f1cbfbc5f24ebf2fb77e092117917690a680
unknown <pugazhmukilanoffical2004@gmail.com>
2025-06-20 12:36:56 +0530

    fixed social media links

--- lib/widgets/floatingactionbutton.dart
+++ lib/widgets/floatingactionbutton.dart
@@ -4,53 +4,115 @@ import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaFAB extends StatelessWidget {
   // Function to launch a URL
-  void _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+  
+
+  @override
+  Widget build(BuildContext context) {
+  //   return FloatingActionButton.extended(
+  //     onPressed: () {}, // You can keep this empty or handle other logic
+  //      // For a rectangular shape
+      
+  //     label:const  Column(
+  //       mainAxisSize: MainAxisSize.min, // Ensures the button only takes the space it needs
+  //       children: [
+          
+          
+  //          SocialMediaWidget(icon:'assets/floating/github.png',url:'https://github.com/pugazhmukilan'),
+  //          SocialMediaWidget(icon:'assets/floating/linkedin.png',url:'https://www.linkedin.com/in/pugazh-mukilan-922206251/'),
+
+  //          SocialMediaWidget(icon:'assets/floating/leetcode.png',url:'https://leetcode.com/u/pugazhmukilanoffical2004/'),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+    //   return RawMaterialButton(
+    //   onPressed: () {},
+    //   fillColor: Colors.blue,
+    //   shape: const CircleBorder(),
+    //   // constraints: BoxConstraints.tightFor(
+    //   //   width: 70.0,
+    //   //   height: 140.0, // set custom height
+    //   // ),
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       SocialMediaWidget(icon:'assets/floating/leetcode.png',url:'https://leetcode.com/u/pugazhmukilanoffical2004/'),
+    //       SocialMediaWidget(icon:'assets/floating/github.png',url:'https://github.com/pugazhmukilan'),
+    //       SocialMediaWidget(icon:'assets/floating/linkedin.png',url:'https://www.linkedin.com/in/pugazh-mukilan-922206251/'),
+    //     ],
+    //   )
+    // );   
+
+    return const Stack(
+        children: [
+          // Your main content here (can be any widget)
+          Positioned(
+            right: 0,
+            bottom: 5, // Adjust the bottom position as needed
+            child:  Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center, // Align children to the right within the column
+              children: [
+                SocialMediaWidget(icon:'assets/floating/leetcode.png',url:'https://leetcode.com/u/pugazhmukilanoffical2004/'),
+                SocialMediaWidget(icon:'assets/floating/github.png',url:'https://github.com/pugazhmukilan'),
+                SocialMediaWidget(icon:'assets/floating/linkedin.png',url:'https://www.linkedin.com/in/pugazh-mukilan-922206251/'),
+              ],
+            ),
+          ),
+        ],
+      );
+  }
+  
+}
+
+class SocialMediaWidget extends StatelessWidget {
+  const SocialMediaWidget({
+    super.key,
+    required this.icon,
+    required this.url,
+  });
+
+  final String icon;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+          icon: SizedBox(
+            width: 40, // Set your desired width
+            height: 40, // Set your desired height
+            child: Image.asset(icon),
+          ),
+          onPressed: () {
+            _launchURL(url);
+          },
+        );
+  }
+}
+
+
+
+void _launchURL(String url) async {
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
     } else {
       throw 'Could not launch $url';
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      onPressed: () {}, // You can keep this empty or handle other logic
-       // For a rectangular shape
-      
-      label: Column(
-        mainAxisSize: MainAxisSize.min, // Ensures the button only takes the space it needs
-        children: [
-          // Facebook Button
-          IconButton(
-            icon: Image.asset('assets/floating/github.png'), // Replace with your asset path
-            iconSize: 10, // Adjust size if needed
-            onPressed: () {
-              _launchURL('https://github.com/pugazhmukilan'); // Redirect to Facebook
-            },
-          ),
-          // Twitter Button
-          IconButton(
-            icon: Image.asset('assets/floating/linkedin.png'), // Replace with your asset path
-            iconSize: 10,
-            onPressed: () {
-              _launchURL('https://www.linkedin.com/in/pugazh-mukilan-922206251/'); // Redirect to Twitter
-            },
-          ),
-          // LinkedIn Button
-          
-          // GitHub Button
-         
 
-          IconButton(
-            icon: Image.asset('assets/floating/leetcode.png'), // Replace with your asset path
-            iconSize: 10,
-            onPressed: () {
-              _launchURL('https://leetcode.com/u/pugazhmukilanoffical2004/'); // Redirect to GitHub
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
+//'assets/floating/leetcode.png'
+
+
+/*RawMaterialButton(
+  onPressed: () {},
+  fillColor: Colors.blue,
+  shape: const CircleBorder(),
+  constraints: BoxConstraints.tightFor(
+    width: 70.0,
+    height: 70.0, // set custom height
+  ),
+  child: Icon(Icons.add, size: 30.0, color: Colors.white),
+) */
